8ccc6b8de4fb1e6bcb23b7ed717fc939c1f4a5ea
tranBao-99 <tranvietbao1999@gmail.com>
2020-11-30 01:41:27 +0700

    clustering

---
+++ clustering/cluster.py
@@ -0,0 +1,86 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.spatial.distance import cdist
+
+means = [[2, 2], [9, 2], [4, 9]]
+cov = [[2, 0], [0, 2]]
+n_samples = 500
+n_cluster = 3
+
+x0 = np.random.multivariate_normal(means[0], cov, n_samples)
+x1 = np.random.multivariate_normal(means[1], cov, n_samples)
+x2 = np.random.multivariate_normal(means[2], cov, n_samples)
+x = np.concatenate((x0, x1, x2), axis=0)
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.plot(x[:, 0], x[:, 1], 'bo', markersize=5)
+plt.plot()
+
+
+# plt.show()
+
+def kmeans_init_centers(X, n_cluster):
+    # random k index beetween 0 and shape(X) without duplicate index.
+    # Then return X[index] as cluster
+    return X[np.random.choice(X.shape[0], n_cluster, replace=False)]
+
+
+# Hàm này dùng để vẽ dữ liệu lên đồ thị
+# Random color chỉ làm việc với k <= 4
+# Nếu bạn thay đổi k > 4, hãy sửa lại phần random color nhé
+# Chỉ sử dụng trong bài toán này thôi nhé.
+def kmeans_visualize(X, centers, labels, n_cluster, title):
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title(title)
+    plt_colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
+
+    for i in range(n_cluster):
+        data = X[labels == i]
+        plt.plot(data[:, 0], data[:, 1], plt_colors[i] + '^', markersize=4, label='cluster_' + str(i))
+        plt.plot(centers[i][0], centers[i][1], plt_colors[i + 4] + 'o', markersize=10, label='center_' + str(i))
+    plt.legend()
+    plt.show()
+
+def kmeans_predict_labels(X, centers):
+  D = cdist(X, centers)
+  # return index of the closest center
+  return np.argmin(D, axis = 1)
+
+def kmeans_update_centers(X, labels, n_cluster):
+  centers = np.zeros((n_cluster, X.shape[1]))
+  for k in range(n_cluster):
+    # collect all points assigned to the k-th cluster
+    Xk = X[labels == k, :]
+    # take average
+    centers[k,:] = np.mean(Xk, axis = 0)
+  return centers
+
+def kmeans_has_converged(centers, new_centers):
+  # return True if two sets of centers are the same
+  return (set([tuple(a) for a in centers]) ==
+      set([tuple(a) for a in new_centers]))
+
+def kmeans(init_centes, init_labels, X, n_cluster):
+  centers = init_centes
+  labels = init_labels
+  times = 0
+  while True:
+    labels = kmeans_predict_labels(X, centers)
+    kmeans_visualize(X, centers, labels, n_cluster, 'Assigned label for data at time = ' + str(times + 1))
+    new_centers = kmeans_update_centers(X, labels, n_cluster)
+    if kmeans_has_converged(centers, new_centers):
+      break
+    centers = new_centers
+    kmeans_visualize(X, centers, labels, n_cluster, 'Update center possition at time = ' + str(times + 1))
+    times += 1
+  return (centers, labels, times)
+
+init_centers = kmeans_init_centers(x, n_cluster)
+print(init_centers)
+init_labels = np.zeros(x.shape[0])
+kmeans_visualize(x, init_centers, init_labels, n_cluster, 'Init centers in the first run. Assigned all data as cluster 0')
+centers, labels, times = kmeans(init_centers, init_labels, x, n_cluster)
+
+print('Done! Kmeans has converged after', times, 'times')
